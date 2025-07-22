97a5339a1b6b011a99debd93642fbfdfe99f1115
Mateus Borges <mateusdantas73@icloud.com>
2025-07-22 14:51:57 -0300

    Adicionar classe User com métodos para exibir informações do usuário

---
+++ docs/User.js
@@ -0,0 +1,19 @@
+class User {
+    constructor(nome, email, nascimento, role, ativo) {
+        this.nome = nome;
+        this.email = email;
+        this.nascimento = nascimento;
+        this.role = role || "estudante";
+        this.ativo = ativo;
+    }
+
+    exibirInfos() {
+        return `${this.nome}, ${this.email}, ${this.role}, ${this.ativo}`;
+    }
+}
+
+const novoUser = new User('Juliana', 'j@j.comm', '2024-01-01', 'estudante', true);
+console.log(novoUser);
+console.log(novoUser.exibirInfos()); // Juliana,
+
+console.log(User.prototype.isPrototypeOf(novoUser)); // Exibe o protótipo da classe User
